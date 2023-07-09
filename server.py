6ee44c42428af9e91e06b6275a65423c00d91b28
Coding Marquice <49505193+GoldenMarquice@users.noreply.github.com>
2023-07-09 12:47:00 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+from config import db
 
 app = Flask(__name__)
 
@@ -21,29 +22,76 @@ def test():
 
 catalog = []
 
+def fix_id(record):
+    record["_id"] = str(record["_id"])
+    return record
+
 @app.get('/api/products')
 def get_products():
     #TODO: read products from DB and return them
-    return json.dumps(catalog)
+    results = []
+    cursor = db.products.find({})
+    for prod in cursor:
+        results.append(fix_id(prod))
+    return json.dumps(results)
 
 
-@app.post('/api/products/count')
+@app.post('/api/products')
 def save_product():
     product = request.get_json()
-    catalog.append(product)
+    db.products.insert_one(product)
 
-    return json.dumps(product)
+    return json.dumps(fix_id(product))
 
 products=[]
 
 @app.get('/api/products/count')
-def add_products():
+def get_products_count():
 
-    count = len(catalog)
-    
+    cursor = db.products.find({})
+    count = 0 
+    for prod in cursor:
+        count +=1
+
     return json.dumps(count)
 
 
+# get /api/categories
+# return the list of categories
+# create an empty list
+# get the products into a cursor
+# travel cursor and get the category of the prod
+# append the category to your list
+# at the end, return the list
+# sort
+
+@app.get("/api/categories")
+def get_categories():
+    cursor= db.products.find({})
+    results = []
+    for prod in cursor: 
+        category = prod["category"]
+        if category not in results:
+            results.append(category)
+
+    results.sort()
+    return json.dumps(results)
+
+@app.get("/api/products/category/<category>")
+def get_products_by_category(category):
+    results = []
+    cursor = db.products.find({})
+    for prod in cursor:
+        if prod["category"] == category:
+            results.append(fix_id(prod))
+    return json.dumps(results)
+
+#create an results list
+# read products into cursor
+# travel the cursor
+#if the product category is equal to the category variable
+# append product to results
+# at the end return results 
 
 # SJC=124388
 # Create an endpoint that return the number of products in the catalog
